8660edf4a93a68cacba7fccb4a1feb1a64907177
Achal-Aggarwal <theachalaggarwal@gmail.com>
2016-03-21 11:57:33 +0530

    Modifying plugin name.

--- src/main/scala/com/thoughtworks/dbmigrator/plugin/HiveMigratorMojo.scala
+++ src/main/scala/com/thoughtworks/dbmigrator/plugin/HiveMigratorMojo.scala
@@ -3,8 +3,9 @@ package com.thoughtworks.dbmigrator.plugin
 import org.apache.maven.plugin.AbstractMojo
 import org.apache.maven.plugins.annotations.Mojo
 
-@Mojo(name = "hello")
-class AppMojo extends AbstractMojo{
+@Mojo(name = "migrate_hive")
+class HiveMigratorMojo extends AbstractMojo{
+
   override def execute() = {
     println( "Hello World!" )
   }
